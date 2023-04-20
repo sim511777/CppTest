6d4fdbe82afb4e275c02cdda889da3e0dee24e0c
Sunghyun Shim <37799084+sim511777@users.noreply.github.com>
2023-04-20 19:38:01 +0900

    ConsoleColor2 추가

--- CppTest/main.cpp
+++ CppTest/main.cpp
@@ -1981,6 +1981,19 @@ void ConsoleColor() {
     cout << "\033[0m" << "reset" << endl;
 }
 
+void ConsoleColor2() {
+    int i, j, n;
+
+    for (i = 0; i < 11; i++) {
+        for (j = 0; j < 10; j++) {
+            n = 10 * i + j;
+            if (n > 108) break;
+            printf("\033[%dm %3d\033[m", n, n);
+        }
+        printf("\n");
+    }
+}
+
 int main() {
     //VariableArgumentTest();
     //OpenMPTest();
@@ -2065,6 +2078,7 @@ int main() {
     //tringEncodingTest2();
     //PredefinedFunctionObject();
     ConsoleColor();
+    ConsoleColor2();
 
     return 0;
 }
